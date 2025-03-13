fc9e84b0881de76ec3085bea745e2d2b551fee40
Juan Correa <gibaros@users.noreply.github.com>
2025-03-13 01:47:23 -0400

    feat(headers): update file headers to Mojaloop Foundation format

--- src/config.js
+++ src/config.js
@@ -1,16 +1,13 @@
-/**************************************************************************
- *  (C) Copyright ModusBox Inc. 2021 - All rights reserved.               *
- *                                                                        *
- *  This file is made available under the terms of the license agreement  *
- *  specified in the corresponding source code repository.                *
- *                                                                        *
- *  ORIGINAL AUTHOR:                                                      *
- *       Murthy Kakarlamudi - murthy@modusbox.com                         *
- *                                                                        *
- *  CONTRIBUTORS:                                                         *
- *       Juan Correa - juan.correa@modusbox.com                           *
- *       Yevhen Kyriukha - yevhen.kyriukha@modusbox.com                   *
- **************************************************************************/
+/*****
+License
+--------------
+Copyright © 2020-2025 Mojaloop Foundation
+The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*****/
 
 const path = require('path');
 require('dotenv/config');
